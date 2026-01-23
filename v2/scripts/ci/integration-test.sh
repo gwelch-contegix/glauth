b1a73b6890c0c05fbf17595a68877665a2c5cd8a
Timmy Welch <gwelch@valiantysfederal.com>
2026-01-23 15:50:23 -0800

    shfmt

--- v2/scripts/ci/integration-test.sh
+++ v2/scripts/ci/integration-test.sh
@@ -4,17 +4,16 @@ export CLEANUP="$1"
 
 ## Main Methods
 
-
 # Dep check
 
-command -v oathtool ;
-if [[ "$?" = "1" ]] ; then
+command -v oathtool
+if [[ "$?" = "1" ]]; then
   echo "Please install oathtool or add it to \$PATH before continuing."
-  exit 1;
+  exit 1
 fi
 
 # Get the git working directory base if CI build dir isn't set
-if [[ "$CI_BUILD_DIR" == "" ]] ; then
+if [[ "$CI_BUILD_DIR" == "" ]]; then
   export CI_BUILD_DIR="$(git rev-parse --show-toplevel)/v2"
 fi
 
@@ -25,19 +24,19 @@ echo "Fixing CI_BUILD_DIR"
 # This script requires that "$CI_BUILD_DIR" is set to the repo root
 
 # Ensure ldap utils are installed (for example - when running this outside of CI)
-if [[ ! `which ldapsearch` ]]; then
-	sudo apt-get -qq update && sudo apt-get -qq install -y ldap-utils || exit 1;
+if [[ ! $(which ldapsearch) ]]; then
+  sudo apt-get -qq update && sudo apt-get -qq install -y ldap-utils || exit 1
 fi
 
 # Display version string
-echo "";
+echo ""
 echo ""
 echo "Version string of tested binary:"
 "$CI_BUILD_DIR/bin/linuxamd64/glauth" --version
 echo ""
 
 # Start in background, capture PID
-"$CI_BUILD_DIR/bin/linuxamd64/glauth" -c "$CI_BUILD_DIR/scripts/ci/test-config.cfg" &> /dev/null &
+"$CI_BUILD_DIR/bin/linuxamd64/glauth" -c "$CI_BUILD_DIR/scripts/ci/test-config.cfg" &>/dev/null &
 
 # Use this instead to see glauth logs while running
 # "$CI_BUILD_DIR/bin/linuxamd64/glauth" -c "$CI_BUILD_DIR/scripts/ci/test-config.cfg" &
@@ -46,16 +45,15 @@ glauthPid="$!"
 echo "Running glauth at PID=$glauthPid"
 
 # Sleep 2 sec, to ensure it comes online successfully and stays up
-sleep 2;
+sleep 2
 
 # Check if process is still running before continuing
-ps aux | grep -v "grep" | grep "$glauthPid" &> /dev/null || FAIL="1"
+ps aux | grep -v "grep" | grep "$glauthPid" &>/dev/null || FAIL="1"
 
-if [[ "$FAIL" = "1" ]] ; then
+if [[ "$FAIL" = "1" ]]; then
   echo "Integration test FAILED - process did not remain running > 2 sec"
-  exit 255;
+  exit 255
 fi
-
 
 FAIL="0"
 
@@ -71,17 +69,16 @@ function bindTest() {
     -D "$1" \
     -w "$2" \
     -x -bdc=glauth,dc=com \
-    "$3" > /dev/null
+    "$3" >/dev/null
 
   exitCode="$?"
 
-  if [[ "$exitCode" = "0" ]] ; then
-    echo "  - PASS : Bind test '$4'";
+  if [[ "$exitCode" = "0" ]]; then
+    echo "  - PASS : Bind test '$4'"
   else
-    echo "  - FAIL : Bind test '$4'";
+    echo "  - FAIL : Bind test '$4'"
     FAIL="1"
   fi
-
 
 }
 
@@ -93,7 +90,7 @@ function snapshotTest() {
   goodResults="$CI_BUILD_DIR/scripts/ci/good-results"
   testResults="$CI_BUILD_DIR/scripts/ci/test-results"
 
-  mkdir "$testResults" &> /dev/null
+  mkdir "$testResults" &>/dev/null
 
   # Run tests here
   ldapCmd="ldapsearch -LLL \
@@ -101,56 +98,54 @@ function snapshotTest() {
     -D cn=serviceuser,ou=svcaccts,dc=glauth,dc=com \
     -w mysecret \
     -x -bdc=glauth,dc=com \
-    $1";
+    $1"
+  go run "$CI_BUILD_DIR/bind-test/main.go"
 
-    # Run the ldap command, pipe to file
-    # "$ldapCmd" &> "$testResults/$2"
+  # Run the ldap command, pipe to file
+  # "$ldapCmd" &> "$testResults/$2"
 
-    # Useful for debugging - output the command run
-    # echo "$ldapCmd";
+  # Useful for debugging - output the command run
+  # echo "$ldapCmd";
 
   ldapsearch -LLL \
     -H ldap://localhost:3893 \
     -D cn=serviceuser,ou=svcaccts,dc=glauth,dc=com \
     -w mysecret \
     -x -bdc=glauth,dc=com \
-    $1 > "$testResults/$2"
+    $1 >"$testResults/$2"
 
+  # Handle the first-run case, saving the test results
+  if [ ! -f "$goodResults/$2" ]; then
+    echo "  - FIRST RUN - SAVING SNAPSHOT, RUN AGAIN TO PASS : '$2'"
 
-    # Handle the first-run case, saving the test results
-    if [ ! -f "$goodResults/$2" ]; then
-      echo "  - FIRST RUN - SAVING SNAPSHOT, RUN AGAIN TO PASS : '$2'";
+    # Copy the results to the goodResults dir, for future runs.
+    cp "$testResults/$2" "$goodResults/$2"
 
-      # Copy the results to the goodResults dir, for future runs.
-      cp "$testResults/$2" "$goodResults/$2";
+    # NOTE: fail=1 must still be set, otherwise CI runs would succeed when they shouldn't
+    FAIL="1"
+  else
 
-      # NOTE: fail=1 must still be set, otherwise CI runs would succeed when they shouldn't
-      FAIL="1"
+    THISFAIL="0"
+    diff -u "$goodResults/$2" "$testResults/$2" || THISFAIL="1"
+
+    if [[ "$CLEANUP" = "cleanup" ]]; then
+      rm -rf "$testResults"
+    fi
+
+    if [[ "$THISFAIL" = "0" ]]; then
+      echo "  - PASS : snapshot '$2'"
     else
-
-      THISFAIL="0"
-      diff -u "$goodResults/$2" "$testResults/$2" || THISFAIL="1"
-
-
-      if [[ "$CLEANUP" = "cleanup" ]] ; then
-        rm -rf "$testResults"
-      fi
-
-    if [[ "$THISFAIL" = "0" ]] ; then
-      echo "  - PASS : snapshot '$2'";
-    else
-      echo "  - FAIL : snapshot '$2'";
+      echo "  - FAIL : snapshot '$2'"
       FAIL="1"
     fi
   fi
 
 }
 
-
-echo "";
-echo "";
+echo ""
+echo ""
 echo "RUNNING TESTS:"
-echo "";
+echo ""
 
 #################
 ## TEST RUNS
@@ -172,7 +167,6 @@ snapshotTest "cn=janedoe" userFetchNonexistentUser1
 # List all posixgroups
 snapshotTest "objectClass=posixgroup" posixGroupList0
 snapshotTest "objectClass=posixaccount" posixAccountList0
-
 
 ## 2FA Bind Test
 # Fetch the OTP for this moment
@@ -210,7 +204,6 @@ bindTest "cn=jackdoe,ou=superheros,dc=glauth,dc=com" \
   "cn=jackdoe" \
   "AppPwNoOtp3"
 
-
 ####
 # Test for a user who also uses OTP
 ####
@@ -244,18 +237,18 @@ bindTest "cn=sarahdoe,ou=superheros,dc=glauth,dc=com" \
 ## Cleanup
 #############
 
-echo "";
-echo "";
+echo ""
+echo ""
 
 # Kill saved PID when done
 # However - throw the fail flag if the process isn't there (ie, exited prematurely)
 echo "Killing glauth"
 kill "$glauthPid" || FAIL="1"
 
-if [[ "$FAIL" = "0" ]] ; then
+if [[ "$FAIL" = "0" ]]; then
   echo "Integration test success"
-  exit 0;
+  exit 0
 else
   echo "Integration test FAILED"
-  exit 255;
+  exit 255
 fi
